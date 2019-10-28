41410eadd644a776efface52fd989866a7ddda97
HtetthuAung283 <htetthuaung283@gmail.com>
2019-10-28 23:38:20 +0630

    first commit

---
+++ gittxt.py
@@ -0,0 +1,10 @@
+git init
+git add
+git config global user.email "useremail"
+git config global user.name "user name"
+git remote add origin http: //github.com/brainseeddeveloper/PYthonHybridClass.git
+git push -u origin master
+
+git add
+git commit -m "second commit"
+git push
---
+++ helloworld.py
@@ -0,0 +1,7 @@
+print("hello world")
+a = 2
+b = 3
+c = a + b
+print(a)
+print(b)
+print(c)
